9fc3ec42cc20b7749a6c1c25377f441a8216b611
Anders Kalhauge <anders@kalhauge.dk>
2018-05-07 11:58:30 +0200

    exercise #1

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,22 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module Main where
 
 import Network.Wai.Middleware.Cors
 import Web.Scotty
+
+import GHC.Generics
+import Data.Aeson (ToJSON, FromJSON)
+
+data Member = Member
+  { id :: Int
+  , name :: String
+  , email :: String
+  } deriving (Show, Generic)
+
+instance ToJSON Member
+
+instance FromJSON Member
 
 main :: IO ()
 main = do
